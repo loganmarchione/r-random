33ee10971e1fb54d180af6af94421a0656e7020c
Logan Marchione <loganmarchione@users.noreply.github.com>
2019-09-25 14:37:21 +0000

    Update to add self-uninstall

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,14 @@
 chrome.browserAction.onClicked.addListener(function(activeTab) {
     chrome.tabs.update({ url: "https://www.reddit.com/r/random" });
 });
+
+function onCanceled(error) {
+  console.log(`Canceled: ${error}`);
+}
+
+var uninstalling = browser.management.uninstallSelf({
+  showConfirmDialog: true,
+  dialogMessage: "I'm retiring this extension. The extension will now attempt a self-uninstall.\n"
+});
+
+uninstalling.then(null, onCanceled);
